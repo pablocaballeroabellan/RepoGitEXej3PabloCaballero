c5112c0251b68e99d5e7699e964b963176016311
pablocaballeroabellan <pablocaballero2smr@gmail.com>
2023-06-14 11:07:48 +0200

    commit and push javadoc

--- RecPabloCaballeroE3/src/claseparacommit2.java
+++ RecPabloCaballeroE3/src/claseparacommit2.java
@@ -1,4 +1,19 @@
 
 public class claseparacommit2 {
+	
+	/** AQUI COMENTANDO PARA HACER UN JAVADOC
+	 * @PEPE ES UNA VARIABLE
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * */
 
 }
